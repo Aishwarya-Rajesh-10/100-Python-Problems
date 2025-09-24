a3ee686cb296192954283013bbdc2be3b76426f3
Aishwarya-Rajesh-10 <aishwaryarajesh126@gmail.com>
2025-09-24 07:12:53 +0530

    Add third program

---
+++ largest of 3.py
@@ -0,0 +1,11 @@
+a=2
+b=3
+c=5
+if a>b and a>c:
+    print("Largest number is a",a)
+elif b>a and b>c:
+    print("Largest number is b",b)
+elif c>a and c>b:
+    print("Largest number is c",c)
+else:
+    ("You have entered a invalild number")
